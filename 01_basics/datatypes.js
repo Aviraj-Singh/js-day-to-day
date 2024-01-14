1f97a4c4f493296e2433456fec5a31e6a9b881d2
Aviraj Singh <69676469+Aviraj-Singh@users.noreply.github.com>
2024-01-14 08:31:32 +0000

    sumarization of different datatypes in JS

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -97,3 +97,43 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// Summarization:
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const scores = 100
+const scoreValue = 100.3
+
+const isLoggedInn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Avi",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
